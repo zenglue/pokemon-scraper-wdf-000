c04131d4918a30b05372fca36363d898908f6c1d
Keo  Bun <keonilrath@gmail.com>
2016-10-14 16:40:11 +0000

    still broken

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
 
 #rspec is passing only one argument?, this passes with only one arg, shouldnt it need at least 3?
-  def initialize(id)
+  def initialize(id, name, type)
     @id = id
     @name = name
     @type = type
@@ -15,6 +15,7 @@ class Pokemon
 
   def self.find(id, db)
     sql = "SELECT * FROM pokemon WHERE id = ?"
-    db.execute(sql, id)[0]
+    result = db.execute(sql, id)[0]
+    Pokemon.new(result[0], result[1], result[2])
   end
 end
